d0cc8c80cb221851f3c43ef6da671f946563c93f
anshita-shrivastava3 <anshita.shrivastava@students.iiit.ac.in>
2022-08-31 15:30:23 +0530

    working copy file code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <cstring>
+#include <string>
 #include <iomanip>
 #include <vector>
 #include <stack>
@@ -11,12 +12,16 @@
 #include <grp.h>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sstream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <termios.h>
+using namespace std;
+
 struct termios orig_termios;    
 char switch_nc='n';
-using namespace std;
+string pwds;
 
 class FileDetails{
 
@@ -71,7 +76,7 @@ void enable_non_can_mode(){
    tcgetattr(STDIN_FILENO, &orig_termios);
    atexit(disable_non_can_mode);
    struct termios raw=orig_termios;
-   raw.c_lflag &= ~(ECHO | ICANON);
+   raw.c_lflag &= ~(/*ECHO | */ICANON);
    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -206,6 +211,8 @@ char print_files(vector<FileDetails> &vec, stack<string> &forward_stack, stack<s
     if(k==10 && vec[curr].f_type=="d"){         //enter into a directory
         if(vec[curr].f_path != backward_stack.top())
             backward_stack.push(vec[curr].f_path);
+        
+        pwds=backward_stack.top();
     }
 
     if(k==27 && y==67){     //right key
@@ -213,13 +220,17 @@ char print_files(vector<FileDetails> &vec, stack<string> &forward_stack, stack<s
             backward_stack.push(forward_stack.top()); //extract top from forward stack and push to backward stack
             forward_stack.pop();   //pop form forward stack
         }
+
+        pwds=backward_stack.top();
     }
 
     if(k==27 && y==68){     //left key
         if(!backward_stack.empty()){
             forward_stack.push(backward_stack.top()); //extract top from backward stack and push to forward stack
             backward_stack.pop();   //pop form backward stack
-        }  
+        } 
+
+        pwds=backward_stack.top(); 
     }
 
     if(k==127){
@@ -308,14 +319,103 @@ string get_home(){
     return getpwuid(getuid())->pw_dir;
 }
 
+vector<string> get_command(string s){
+    stringstream temp(s);
+    vector<string> command;
+    string t;
+    while (getline(temp, t, ' ')) {  
+        command.push_back(t);
+    }  
 
-string refresh_dir_command(string pwd){
-    clear_terminal();
+    return command;
+}
+
+string get_absolute_path(string rel_path){
+    string ret;
+    if(rel_path[0]=='~'){
+        ret=get_home();
+        rel_path=rel_path.substr(1);
+        ret+=rel_path;
+    }else if(rel_path[0]=='.'){
+        ret=pwds;
+        rel_path=rel_path.substr(1);
+        ret+=rel_path;
+    }else if(rel_path[0]=='/'){
+        ret=rel_path;
+    }
+
+    return ret;
+}
+
+void copy(string source_file, string destination_directory){
+    char buf;
+    int fd_one, fd_two;
+    string f, s;
+    int pos;
+    
+    // if(destination_directory[0]='~'){
+    //     s=get_home();
+    //     destination_directory=destination_directory.substr(1);
+    //     s+=source_file;
+    //     cout<<"value of source file: "<<f<<endl;
+    // }
+    f=get_absolute_path(source_file);
+    s=get_absolute_path(destination_directory);
+    
+    pos = f.find_last_of("/");
+    s=s+f.substr(pos);
+
+    // cout<<"value of source file: "<<f<<endl;
+    // cout<<"value of dest file: "<<s<<endl;
+
+    fd_one = open(f.c_str(), O_RDONLY);
+    if (fd_one == -1)
+    {
+        printf("Error opening first_file\n");
+        close(fd_one);
+    }
+    fd_two = open(s.c_str(), 
+                O_WRONLY | O_CREAT,
+                S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    while(read(fd_one, &buf, 1))
+    {
+        write(fd_two, &buf, 1);
+    }
+
+    //printf("Successful copy");
+
+    close(fd_one);
+    close(fd_two);
+}
+
+string refresh_dir_command(){
+    //clear_terminal();
     cout<<"\n";
-    set_status(pwd, 2);
-    cout<<"\nType command: ";
+    set_status(pwds, 2);
+    char ch;
     string s;
-    cin>>s;
+
+    //cout<<"Enter ch: ";
+    ch=cin.get();
+    if(ch==27){
+        switch_nc='n';
+        cout<<"escape";
+        return "nm";
+    }
+    
+    disable_non_can_mode();
+    getline(cin, s);
+    s= ch + s;
+    //copy foo.txt bar.txt baz.mp4 ~/foobar
+    vector<string> command=get_command(s);
+    if(command[0]=="copy"){
+        int n=command.size();
+        for(int i=1; i<n-1; ++i){
+            cout<<"command[i]: "<<command[i]<<" command[n-1] "<<command[n-1]<<endl;
+            copy(command[i], command[n-1]);
+        }
+    }
+    enable_non_can_mode();
     return s;
 }
 
@@ -323,7 +423,8 @@ int main(){
     enable_non_can_mode();
     stack<string> forward_stack;
     stack<string> backward_stack;
-    backward_stack.push(get_home());
+    pwds=get_home();
+    backward_stack.push(pwds);
     char c='a';
     string sc="sc";
     do{  
@@ -332,9 +433,7 @@ int main(){
         }
 
         if(switch_nc=='c'){
-          //  cout<<"switched to command mode\n";
-            sc=refresh_dir_command(backward_stack.top());
-           //cout<<"control returns :"<<sc<<endl;
+            sc=refresh_dir_command();
         }
         
         if(sc!="quit"){
@@ -343,7 +442,7 @@ int main(){
         if(c!='q'){
             c='a';
         }
-        clear_terminal();
+        //clear_terminal();
     }while(c != 'q' && sc != "quit");
  
     return 0;
